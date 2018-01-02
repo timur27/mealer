2dd0aedcf1d28b4736849a826d9215597ae606cb
Matthew Krzys <matthewkrzys@gmail.com>
2018-01-02 20:43:14 +0100

    First Commit

---
+++ src/main/java/com/mealer/mealer/Controller.java
@@ -0,0 +1,14 @@
+package com.mealer.mealer;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Mealer";
+    }
+
+}
